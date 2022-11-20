5737f3b2b9440a6c509e226a6c8abf530490f378
p0lygun <solankivibhakar82@gmail.com>
2022-11-20 23:25:06 +0530

    chore: prepare for merge with main

--- libs/gen_docs.py
+++ libs/gen_docs.py
@@ -21,23 +21,26 @@ for file in (project_dir / "docs_json").glob("*.json"):
     with file.open('r') as JSON_FILE, (build_dir / (file.stem + ".md")).open('w') as DOC_FILE:
         doc = [
             f"""---
-title: "{file.stem.capitalize()}"
+title: "{file.stem}"
 draft: false
 ---"""
         ]
         doc_json: CleanDoc = json.loads(JSON_FILE.read())
-        doc.append(f"# {doc_json['block']}")
+        # doc.append(f"# {doc_json['block']}")
         doc.append(f"{doc_json['summary']}")
-        doc.append("### Inputs")
         if doc_json.get('inputs', False):
+            doc.append("### Inputs")
             doc.append("\n".join(doc_json['inputs']))
         if doc_json.get('output', False):
+            doc.append("### Output")
             doc.append("\n".join(doc_json['output']))
         doc.append(
             f"\n![{file.stem}](https://raw.githubusercontent.com/battlefield-portal-community/Image-CDN/main/portal_blocks/{file.stem}.png)"  # noqa
         )
-        doc.append("## Extra Documentation")
-        doc.append(doc_json['extra'])
+        if extra := doc_json['extra']:
+            doc.append("## Extra Documentation")
+            doc.append(extra)
+
         DOC_FILE.write("\n".join(doc))
         logger.info(f"build {file.stem}")
 
