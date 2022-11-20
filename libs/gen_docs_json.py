d0f3b1465bf2f3f2d29d3a29fcaa7bed2de59e6b
p0lygun <solankivibhakar82@gmail.com>
2022-11-20 23:30:57 +0530

    chore: complete merge of main into self

--- libs/gen_docs_json.py
+++ libs/gen_docs_json.py
@@ -1,24 +1,49 @@
 import json
+import operator
 import re
+from typing import TypedDict, Optional
+
+import box
+from box import Box
 from loguru import logger
 
-from helper import project_dir, CleanDoc
+from helper import project_dir
+
+
+class CleanDoc(TypedDict):
+    block: str
+    summary: str
+    inputs: Optional[list]
+    output: Optional[list]
+
+
+def get_nested_dot(dot_string) -> str:
+    try:
+        return operator.attrgetter(dot_string)(i18n)
+    except box.BoxKeyError:
+        if block := mapped_translations.get(dot_string, False):
+            return block
+        raise
 
 
 def gen_text(doc: str, rule_block: bool = False) -> str:
-    if key := re.search('^#+\s*%{(.*?)}', doc):
-        if rule_block:
-            doc = doc.replace(key.group(), f'**{mapped_translations[key.groups()[0]]}**')
+    if key := re.search(r'^#+\s*%{(.*?)}$', doc):
+        mkey = key.groups()[0]
+        if r := mapped_translations.get(mkey, False):
+            replace_with = r
         else:
-            doc = doc.replace(key.group(), f'{mapped_translations[key.groups()[0]]}')
-    elif key := re.search('\*\*%{(.*?)}\*\*', doc):
-        doc = doc.replace(key.group(), f'**{mapped_translations[key.groups()[0]]}**')
-    elif key := re.search('^%{(.*?)}', doc):
-        mkey = key.groups()[0]
-        doc = doc.replace(key.group(), f'{mapped_translations[mkey]}')
-    elif key := re.search('%{(.*?)}', doc):
-        mkey = key.groups()[0]
-        doc = doc.replace(key.group(), f'**{mapped_translations[mkey]}**')
+            replace_with = get_nested_dot(mkey)
+        if rule_block:
+            doc = doc.replace(key.group(), f'**{replace_with}**')
+        else:
+            doc = doc.replace(key.group(), f'{replace_with}')
+
+    elif key := re.search(r'\*\*%{(.*?)}\*\*', doc):
+        doc = doc.replace(key.group(), f'**{get_nested_dot(key.groups()[0])}**')
+    elif key := re.search(r'^%{(.*?)}', doc):
+        doc = doc.replace(key.group(), get_nested_dot(key.groups()[0]))
+    elif key := re.search(r'%{(.*?)}', doc):
+        doc = doc.replace(key.group(), f'**{get_nested_dot(key.groups()[0])}**')
     else:
         return doc
     return gen_text(doc)
@@ -27,8 +52,11 @@ def gen_text(doc: str, rule_block: bool = False) -> str:
 # logger.debug("Get Translations from gametools")
 # translations = requests.get("https://api.gametools.network/bf2042/translations/").json()['localizedTexts']
 
+
 with open(project_dir / "data" / "translations.json") as file:
     translations = json.load(file)['localizedTexts']
+
+i18n = Box.from_json(filename=project_dir / "data" / "i18n.json")
 
 mapped_translations = dict()
 
@@ -36,9 +64,13 @@ logger.debug("Generate Mapping")
 for item in translations:
     mapped_translations[item['sid']] = item['localizedText']
 
-
 logger.info("Generate Docs")
 clean_names = []
+
+# delete all present docs before making new docs
+logger.info("Deleting currently present docs")
+for file in (project_dir / "docs").glob("*.json"):
+    file.unlink()
 
 for raw_doc in sorted((project_dir / "data" / "raw_docs").glob("*.md")):
     bad_blocks = ['controls_if_else', 'missingActionBlockType_v1', 'missingValueBlockType_v1']
@@ -75,9 +107,9 @@ for raw_doc in sorted((project_dir / "data" / "raw_docs").glob("*.md")):
 
     clean_doc['summary'] = "\n".join(clean[1:(i if i else o if o else None)])
     if i:
-        clean_doc['inputs'] = clean[i+1:(o if o else None)]
+        clean_doc['inputs'] = clean[i + 1:(o if o else None)]
     if o:
-        clean_doc['output'] = clean[o+1:]
+        clean_doc['output'] = clean[o + 1:]
 
     k = f"ID_ARRIVAL_BLOCK_{raw_doc.stem.upper()}"
     clean_name = mapped_translations.get(k, False)
@@ -91,24 +123,8 @@ for raw_doc in sorted((project_dir / "data" / "raw_docs").glob("*.md")):
 
     if clean_name in ["RULE", "MOD"]:
         clean_name = clean_name.capitalize()
-
-    doc_json_folder_path = project_dir / "docs_json"
-    doc_json_folder_path.mkdir(exist_ok=True)
-    doc_json_file_path = doc_json_folder_path / f'{clean_name}.json'
-    doc_json_file_path.touch(exist_ok=True)
-
-    with open(doc_json_file_path, 'r+') as file:
-        try:
-            doc_json: CleanDoc = json.loads(file.read())
-        except json.JSONDecodeError:
-            doc_json = dict()
-        doc_json.update(clean_doc)
-        if not doc_json.get('extra', False):
-            doc_json['extra'] = ''
-        file.seek(0)
-        json.dump(doc_json, file, indent=4)
-        file.truncate()
-
+    with open(project_dir / "docs" / f'{clean_name}.json', 'w') as file:
+        json.dump(clean_doc, file)
     clean_names.append(clean_name)
     logger.debug(f"{raw_doc.stem} -> {clean_name}")
 
