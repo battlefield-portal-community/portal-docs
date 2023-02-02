3088b39caf5690dbf5c15f57b5f9fe7d99051943
p0lygun <solankivibhakar82@gmail.com>
2023-02-02 12:07:46 +0530

    fix: import gen_json after i18n has been populated

--- libs/json_docs_generator.py
+++ libs/json_docs_generator.py
@@ -5,7 +5,6 @@ load_dotenv()
 if __name__ == '__main__':
     from gen_data import gen_data  # order matters because of monkey patching
     from get_block_names import get_block_names
-    from gen_docs_json import gen_json
 
     print("::group::Get Block Names")
     get_block_names()
@@ -14,6 +13,7 @@ if __name__ == '__main__':
     gen_data()
     print("::endgroup::")
     print("::group::Generate Docs Json")
+    from gen_docs_json import gen_json  # import later as i18n needs to be populated with the latest changes
     gen_json()
     print("::endgroup::")
 
