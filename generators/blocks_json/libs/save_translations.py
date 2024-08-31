4ccae4403d079e0c3b6253190c553b41e14bc0ed
MagneticNeedle <vibhakar.solanki@magnifi.ai>
2024-08-31 16:40:28 +0530

    feat(block_json): save translations file only when there is a change

--- generators/blocks_json/libs/save_translations.py
+++ generators/blocks_json/libs/save_translations.py
@@ -1,6 +1,9 @@
 import json
 
 import requests
+
+from deepdiff import DeepHash
+
 from generators.utils import skip_function_if_env
 from . import config
 from loguru import logger
@@ -9,14 +12,16 @@ TRANSLATION_FILE = config.DATA_DIR / "translations.json"
 
 
 @skip_function_if_env("SKIP_TRANSLATIONS")
-def save_translations_json(indent: int = 4):
+def save_translations_json(indent: int = 4) -> bool:
     logger.debug("Saving Translations")
-    with open(TRANSLATION_FILE, "w") as file:
-        json.dump(
-            requests.get("https://api.gametools.network/bf2042/translations/").json(),
-            file,
-            indent=indent,
-        )
+    upstream_json = requests.get("https://api.gametools.network/bf2042/translations/").json()
+    local_json = json.load(open(TRANSLATION_FILE))
+    if DeepHash(upstream_json)[upstream_json] == DeepHash(local_json)[local_json]:
+        logger.info("No changes in translations")
+        return False
+
+    with open(TRANSLATION_FILE, "w") as f:
+        json.dump(upstream_json, f, indent=indent)
     return True
 
 
